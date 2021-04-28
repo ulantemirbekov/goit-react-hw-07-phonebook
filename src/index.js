afe752fe8684e170c5b519553eaec23de3542284
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-29 00:09:22 +0300

    index updated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './components/App';
-import store from './redux/store';
+import {HashRouter as Router} from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import {store,persistor } from './redux/store';
+import 'modern-normalize/modern-normalize.css';
 import './index.css';
+import App from './App';
 
 ReactDOM.render(
+  <React.StrictMode>
     <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root'),
+      <Router>      
+      <PersistGate loading={null} persistor={persistor}> 
+        <App />    
+       </PersistGate>
+      </Router>
+    </Provider>
+  </React.StrictMode>,
+
+  document.getElementById('root'),
 );
